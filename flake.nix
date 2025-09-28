33d59daa6267a616ce741d30d044e701ece0fc2b
Justin Moon <mail@justinmoon.com>
2025-09-28 20:15:30 +0100

    Point flake to moq rs subflake

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    moq.url = "github:kixelated/moq";
+    moqrs.url = "github:kixelated/moq?dir=rs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, moq, ... }:
+  outputs = { self, nixpkgs, flake-utils, moqrs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        moqPkgs = moq.packages.${system};
+        moqPkgs = moqrs.packages.${system};
       in
       {
         packages = {
